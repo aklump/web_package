d48c9a203ef055d40adaf7638cd2b8e2d4b51483
aklump <sourcecode@intheloftstudios.com>
2014-09-25 16:26:11 -0700

    updated build files with better jquery building.

---
+++ template/examples/2_demo_compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 
+# @file
+# Copy relevent files from parent into demo
+# 
+cp "$7/jquery.photo_essay.js" "$7/demo/"
+cp "$7/jquery.photo_essay.min.js" "$7/demo/"
+cp "$7/photo_essay.css" "$7/demo/"
+cp "$7/images/loading.gif" "$7/demo/images/"
+cp "$7/images/loading-sm.gif" "$7/demo/images/"
