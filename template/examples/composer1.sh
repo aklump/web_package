f22909b63f43bbabf18dacc54adebd831f95049b
aklump <sourcecode@intheloftstudios.com>
2014-10-23 12:19:43 -0700

    Added examples for composure update on build.

---
+++ template/examples/composer1.sh
@@ -0,0 +1,2 @@
+# This is meant to be included with composer2.sh
+echo 'Checking composer dependencies...'
---
+++ template/examples/composer2.sh
@@ -0,0 +1,2 @@
+# You should include composer1.sh as well
+cd $7 && /Volumes/Data/Users/aklump/bin/composer update
