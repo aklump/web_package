e9b88a6ef94a931b13221031dd107a000c6c6742
Aaron Klump <sourcecode@intheloftstudios.com>
2018-07-05 13:49:01 -0700

    Improve clarity of missing git_root variable.

--- includes/preroute.sh
+++ includes/preroute.sh
@@ -19,5 +19,21 @@ if [ "$lobster_op" == 'info' ]; then
 fi
 
 if [ "$wp_git_root" ] && ! test -e $wp_git_root/.git; then
-  lobster_warning "Git root $wp_git_root/.git does not exist; check your config file for git_root"
+    wp_git_root="$( cd -P "$( dirname "$wp_git_root" )" && pwd )"
+    lobster_echo
+    lobster_warning "Configuration says the git root should be here: \"$wp_git_root/.git\", however it does not exist."
+    lobster_echo
+    lobster_warning "- Do you need to run \"cd $wp_git_root/ && git init\"?"
+    lobster_warning "- Is the path in the config (git_root) incorrect?"
+    lobster_warning "- If you're not using Git, please remove (git_root) from your config file."
+    lobster_echo
 fi
+
+
+source="${BASH_SOURCE[0]}"
+while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
+  dir="$( cd -P "$( dirname "$source" )" && pwd )"
+  source="$(readlink "$source")"
+  [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+root="$( cd -P "$( dirname "$source" )" && pwd )"
