bf26569dbaf375d88bc0f6b36d4094700f010883
Aaron Klump <sourcecode@intheloftstudios.com>
2018-07-05 16:24:43 -0700

    Update example code.

--- install/template/examples/00_composer_update.sh
+++ install/template/examples/00_composer_update.sh
@@ -2,6 +2,6 @@
 #
 # @file Do a composer update
 #
-# You should include composer1.sh as well so the note prints before the delay begins.
+
 composer=$(type composer >/dev/null 2>&1 && which composer)
 cd "$7" && $composer update
