91e0798710823731398e3374cd5ee003debdf501
Aaron Klump <sourcecode@intheloftstudios.com>
2018-12-04 15:25:53 -0800

    Compile docs.

---
+++ .web_package/hooks/build/00_composer.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+composer update
+composer dumpautoload --optimize
--- .web_package/hooks/build/00_composer_update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# @file Do a composer update
-#
-# You should include composer1.sh as well so the note prints before the delay begins.
-composer=$(type composer >/dev/null 2>&1 && which composer)
-cd "$7" && $composer update
