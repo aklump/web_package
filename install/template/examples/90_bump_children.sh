4c1d02fa33c813081206892c8d93580da5747f7d
Aaron Klump <sourcecode@intheloftstudios.com>
2019-08-16 18:56:06 -0700

    improve bump children example

--- install/template/examples/90_bump_children.sh
+++ install/template/examples/90_bump_children.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #
 # @file
-# Applies the build process to child projects
+# Applies the build process to child projects.
 #
-web_package=$(type web_package >/dev/null 2>&1 && which web_package)
-if [ "$web_package" ]; then
-  (cd $7/web/sites/all/themes/gop5_theme && $web_package build)
-  (cd $7/web/sites/all/modules/custom/gop3_core && $web_package build)
-  (cd $7/web/sites/all/themes/gop5_theme/guides && $web_package build)
+bump=$(type web_package >/dev/null 2>&1 && which web_package)
+if [ "$bump" ]; then
+  echo "building children"
+  (cd $7/web/sites/all/modules/custom/gop3_core && $bump build) || build_fail_exception
+  (cd $7/web/sites/all/modules/custom/gop3_media && $bump build) || build_fail_exception
+  (cd $7/web/sites/all/themes/gop5_theme && $bump build) || build_fail_exception
 fi
