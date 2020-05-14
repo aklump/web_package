ae98271d154ac6418963b476075855f777e2e3db
Aaron Klump <sourcecode@intheloftstudios.com>
2020-05-13 19:01:14 -0700

    Update examples.

--- install/template/examples/00_composer_install.sh
+++ install/template/examples/00_composer_install.sh
@@ -4,7 +4,7 @@
 # This file should be used when you are committing the vendor directory.
 #
 
-composer install --no-dev && composer dumpautoload --optimize && git add composer.lock && git add vendor|| build_fail_exception
+composer install --no-dev && composer dumpautoload --optimize && git add composer.lock && git add vendor || build_fail_exception
 
 # This is for a specific version of PHP.
 #/Applications/MAMP/bin/php/php5.6.32/bin/php  /Users/aklump/bin/composer install --no-dev && composer dumpautoload --optimize && git add composer.lock && git add vendor || build_fail_exception
--- install/template/examples/90_bump_children.sh
+++ install/template/examples/90_bump_children.sh
@@ -6,7 +6,7 @@
 bump=$(type web_package >/dev/null 2>&1 && which web_package)
 if [ "$bump" ]; then
   echo "building children"
-  (cd $7/web/sites/all/modules/custom/gop3_core && $bump build) || build_fail_exception
-  (cd $7/web/sites/all/modules/custom/gop3_media && $bump build) || build_fail_exception
-  (cd $7/web/sites/all/themes/gop5_theme && $bump build) || build_fail_exception
+  (cd "$7/web/sites/all/modules/custom/gop3_core" && $bump build) || build_fail_exception
+  (cd "$7/web/sites/all/modules/custom/gop3_media" && $bump build) || build_fail_exception
+  (cd "$7/web/sites/all/themes/gop5_theme" && $bump build) || build_fail_exception
 fi
