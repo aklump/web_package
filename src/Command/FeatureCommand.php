44618173aa8fa18f52dff1c4a402acae57f07cde
Aaron Klump <sourcecode@intheloftstudios.com>
2023-10-13 13:23:06 -0700

    Fix missing FQN.

--- src/Command/FeatureCommand.php
+++ src/Command/FeatureCommand.php
@@ -11,6 +11,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use AKlump\WebPackage\Config\Config;
 
 /**
  * @url https://nvie.com/posts/a-successful-git-branching-model/#feature-branches
