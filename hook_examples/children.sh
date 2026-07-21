7ef3c7d0375da70ceec6b87e11c7c16b6ca16ca9
Aaron Klump <sourcecode@intheloftstudios.com>
2026-07-21 15:46:46 -0700

    refactor: Example hooks

--- hook_examples/children.sh
+++ hook_examples/children.sh
@@ -1,21 +1,21 @@
-#!/usr/bin/env bash
-
 #
 # @file Run `bump build` in all custom child extensions (custom module, custom theme, etc.)
 #
 
-# List all extensions that use web_package to build here.
+# ========= Begin configuration =========
+export WEB_PACKAGE_PHP=/usr/local/opt/php@8.1/bin/php
+bump="$HOME/Code/Packages/cli/web-package/app/web-package"
 declare -a children=("./web/modules/custom/my_module" "./web/themes/custom/my_theme")
+# ========= End configuration =========
 
-bump=$(type web_package >/dev/null 2>&1 && which web_package)
 [ "$bump" ] || exit 255
 echo "BUILDING CHILDREN"
 for path in "${children[@]}" ; do
-  echo "├── $(basename $path)"
+  echo "├── 🧩 $(basename $path)"
   if [ -d "$path/.web_package" ]; then
     (cd "$path" && $bump build) || exit 1
   fi
 done
 
 ## Do anything else custom here, e.g. `./bin/build.css.sh`
-#(cd "./web/themes/custom/my_other_theme" && ./bin/build_css.sh) || exit 1
+#(cd "./web/themes/custom/my_other_theme" && ./bin/build_css.sh && echo "CSS Built") || exit 1
